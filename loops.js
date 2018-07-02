953727ea91b5416bfad9e98a8ede5910d1c0926e
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-02 23:54:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,8 @@
 function forLoop(array) {
   for (let i = 1; i < 25; i+++) 
   if (i === 1) {
-    array.push("I am ")
+    array.push("I am 1 strange loop")
+  } else {
+    array.push("I ")
   }
   return array }
